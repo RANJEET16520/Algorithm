36fbb6a760ea274546f4bf4cb0f26798c6237864
RANJEET16520 <ranjeetwalia2000@gmail.com>
2020-11-21 20:06:50 +0530

    ok

---
+++ Permutations/All_Permutatuion_of_Array.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pii pair<int, int>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define cls(g,x) memset(g, x, sizeof(g))
+#define all(x) x.begin(), x.end()
+#define siz(x) x.size()
+#define REP(i,a,b) for(int i=a; i<b; i++)
+const int MAX = LLONG_MAX;
+const int MIN = LLONG_MIN;
+const int MOD = 1e9+7;
+
+/*
+    Submitted By: Ranjeet Walia
+*/
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
+
